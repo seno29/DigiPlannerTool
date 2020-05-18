932dbd081db803c9876968dbc429cf2cc09399e4
Diksha Gupta <dikshagupta2012@gmail.com>
2020-05-18 20:36:03 +0530

    added login,admin-board and user-board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { AdminBoardComponent } from './admin-board/admin-board.component';
+import { UserBoardComponent } from './user-board/user-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    UserComponent,
+    AdminBoardComponent,
+    UserBoardComponent
   ],
   imports: [
     BrowserModule

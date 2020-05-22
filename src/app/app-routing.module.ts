4656396211a7fffadbcd945b06b7ccadcf6d9948
Diksha Gupta <dikshagupta2012@gmail.com>
2020-05-22 08:58:45 +0530

    added view board component to view existing boards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { UserBoardComponent } from './user-board/user-board.component';
 import { AdminBoardComponent } from './admin-board/admin-board.component';
+import { ViewBoardsComponent } from './view-boards/view-boards.component';
 
 const routes: Routes = [
   { path:'login', component: LoginComponent },
   { path:'home', component: HomeComponent },
   { path:'userboard', component: UserBoardComponent },
-  { path:'adminboard', component: AdminBoardComponent }
+  { path:'adminboard', component: AdminBoardComponent },
+  { path:'viewboard' ,component: ViewBoardsComponent}
 ];
 
 @NgModule({
---
+++ src/app/view-boards/view-boards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-boards',
+  templateUrl: './view-boards.component.html',
+  styleUrls: ['./view-boards.component.css']
+})
+export class ViewBoardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

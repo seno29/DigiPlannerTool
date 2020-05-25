8fa7740becac28d870047881be4125392e26dc9b
cannizarro <asrarul97@gmail.com>
2020-05-25 21:29:44 +0530

    Removed previous repetitive code

--- src/app/user-board-services/scaling.service.ts
+++ src/app/user-board-services/scaling.service.ts
@@ -14,32 +14,8 @@ export class ScalingService {
     let resize = false;
     const height = shape.height * shape.scaleY;
     const width = shape.width * shape.scaleX;
-    
-    
-    if (height < textBoundingRect.height  ){
-      if (shape instanceof fabric.Image){
-        const scale = (textBoundingRect.height + 80) / 512;
-        shape.scaleY = scale;
-        shape.scaleX = scale;
-      }
-      else{
-        shape.height = textBoundingRect.height + 20;
-      }
-      resize = true;
-    }
-    if (width < textBoundingRect.width){
-     
-      if (shape instanceof fabric.Image){
-        const scale = (textBoundingRect.width + 80) / 512;
-        shape.scaleY = scale;
-        shape.scaleX = scale;
-      }
-      else{
-        shape.width = textBoundingRect.width+ 20;
-               
-      }
-      resize = true;
-    }
+    resize = this.compareTextShape(shape, height, textBoundingRect.height);
+    resize = this.compareTextShape(shape, width, textBoundingRect.width);
 
     if (shape instanceof fabric.Ellipse && resize){
       if (shape.width < textBoundingRect.width ){
@@ -53,5 +29,25 @@ export class ScalingService {
       shape.rx = shape.width / 2;
       shape.ry = shape.height / 2;
     }
+  }
+
+  compareTextShape(shape: fabric.Shape, shapeDimen: number, textDimen: number): boolean{
+    if (shapeDimen < textDimen  ){
+      if (shape instanceof fabric.Image){
+        const scale = (textDimen + 80) / 512;
+        shape.scaleY = scale;
+        shape.scaleX = scale;
+      }
+      else{
+        if ((shape.height * shape.scaleY) === shapeDimen){
+          shape.height = textDimen + 20;
+        }
+        else{
+          shape.width = textDimen + 20;
+        }
+      }
+      return true;
+    }
+    return false;
   }
 }

4d8eda6465a082b2516128d90c7bf465afb2753d
Asraar <42109962+cannizarro@users.noreply.github.com>
2020-05-25 23:14:32 +0530

    Duplicate Statements

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,7 +10,6 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 

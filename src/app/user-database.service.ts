b16983d172196ffdde0f6a82a110e2578e10c8d5
kiran0605 <kdambokar_b17@it.vjti.ac.in>
2020-05-28 09:37:43 +0530

    updated with constants file

--- src/app/user-database.service.ts
+++ src/app/user-database.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { URI } from './constants';
 @Injectable({
   providedIn: 'root'
 })
 export class UserDatabaseService {
-  private uri: string = 'http://localhost:4200';
   roomData: Observable<string> ;
   constructor(private http: HttpClient) { }
 
   getRoomData(code: string): Observable<string> {
-   return this.roomData = this.http.get( `${this.uri}/drawing/${code}`) as Observable<string>;
+   return this.roomData = this.http.get( `${URI}/drawing/${code}`) as Observable<string>;
   }
 }

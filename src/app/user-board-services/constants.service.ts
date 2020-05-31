f29fa15a0951b822385e60ad5dadc5b63f535427
kiran0605 <kdambokar_b17@it.vjti.ac.in>
2020-05-31 14:08:27 +0530

    minor css changes

--- src/app/user-board-services/constants.service.ts
+++ src/app/user-board-services/constants.service.ts
@@ -23,7 +23,7 @@ export class ConstantsService {
     this.URI = `http://localhost:${this.PORT}`;
     this.clientId = '610664320073-4ik734pbbflijv056jr130n5k6e7ia8q.apps.googleusercontent.com';
     this.starIconURL = '../assets/stars-black-48dp.svg';
-    this.colors = ['CornflowerBlue', 'darkcyan', 'mediumpurple', 'salmon', 'coral', 'gold' ];
+    this.colors = ['CornflowerBlue', 'darkcyan', 'MediumAquaMarine', 'lemonchiffon', 'gold', 'silver', 'salmon', 'palevioletred', 'pink'];
     this.connectText = 'Connect';
     this.disconnectText = 'Exit Connection Mode';
    }

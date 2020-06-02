22986dbf48ef156023706d2bd9a1d07686543937
Diksha Gupta <dikshagupta2012@gmail.com>
2020-06-02 09:41:07 +0530

    writing unit test

--- src/app/board.service.spec.ts
+++ src/app/board.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { BoardService } from './board.service';
 
 describe('BoardService', () => {
   let service: BoardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BoardService],
+    });
     service = TestBed.inject(BoardService);
   });
 

bb52d91fdc8c451b54aaf8fdfa7bedc40d1b6287
Diksha Gupta <dikshagupta2012@gmail.com>
2020-06-02 17:40:06 +0530

    updated port

--- src/app/user-board-services/constants.service.ts
+++ src/app/user-board-services/constants.service.ts
@@ -21,7 +21,7 @@ export class ConstantsService {
   constructor() {
     this.HideControls = { tl: true, tr: false, bl: true, br: true, ml: true, mt: true, mr: true, mb: true, mtr: true };
     this.userBackURL = '../../assets/user_back.png';
-    this.PORT = '4200';
+    this.PORT = '8080';
     this.URI = `http://localhost:${this.PORT}`;
     this.clientId = '610664320073-4ik734pbbflijv056jr130n5k6e7ia8q.apps.googleusercontent.com';
     this.starIconURL = '../assets/stars-black-48dp.svg';

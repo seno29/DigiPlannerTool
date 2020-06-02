99c0480e4cae31abab4ae11a1d5ad6feb186459e
kiran0605 <kdambokar_b17@it.vjti.ac.in>
2020-06-02 12:25:06 +0530

    Api changes made

--- src/app/user-board/user-board.component.ts
+++ src/app/user-board/user-board.component.ts
@@ -35,7 +35,6 @@ export class UserBoardComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.constants.roomID = this.route.snapshot.queryParamMap.get('room_code') || 'unknown';
-   // this.constants.userID = this.route.snapshot.queryParamMap.get('email') || 'unknown';
     this.canvas = this.shapeService.initCanvas(this.renderer);
     this.userSocketService.init(this.canvas, this.renderer, this.constants.roomID);
     this.authService.authState.subscribe((user) => {

5d7f4f4a46d83657fa287c2a5fdfa247c690e992
Diksha Gupta <dikshagupta2012@gmail.com>
2020-06-02 19:32:07 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 const PORT:string = '8080';
-const URI:string = `http://0.0.0.0:${PORT}`;
+const URI:string = `http://localhost:${PORT}`;
 //const clientId:string='610664320073-4ik734pbbflijv056jr130n5k6e7ia8q.apps.googleusercontent.com';
 const clientId:string = '610664320073-4oui7dgr99meb3n28m5ljp25f65fmf79.apps.googleusercontent.com';
 export {URI,clientId};

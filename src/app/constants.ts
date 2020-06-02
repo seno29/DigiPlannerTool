07867ed196db0e779df20ce6cd16ee80a22ffe6d
Diksha Gupta <dikshagupta2012@gmail.com>
2020-06-02 17:08:46 +0530

    new client id for http://localhost:8080

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 const PORT:string = '4200';
 const URI:string = `http://localhost:${PORT}`;
-const clientId:string='610664320073-4ik734pbbflijv056jr130n5k6e7ia8q.apps.googleusercontent.com';
-
+//const clientId:string='610664320073-4ik734pbbflijv056jr130n5k6e7ia8q.apps.googleusercontent.com';
+const clientId:string = '610664320073-8c11uub4p1ojc6agn6sp76jn88bk43ut.apps.googleusercontent.com';
 export {URI,clientId};

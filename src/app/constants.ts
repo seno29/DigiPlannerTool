26250111cd58075768df1e050db9ace0aaebadad
Diksha Gupta <dikshagupta2012@gmail.com>
2020-06-02 17:39:21 +0530

    updated port

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-const PORT:string = '4200';
+const PORT:string = '8080';
 const URI:string = `http://localhost:${PORT}`;
 //const clientId:string='610664320073-4ik734pbbflijv056jr130n5k6e7ia8q.apps.googleusercontent.com';
 const clientId:string = '610664320073-8c11uub4p1ojc6agn6sp76jn88bk43ut.apps.googleusercontent.com';
